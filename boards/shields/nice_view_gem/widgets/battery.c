0aa83fd560d67c2a356893c59fd810b93247549b
Sunjung Kim <51713430+skim-1@users.noreply.github.com>
2026-05-06 02:25:33 -0400

    Update battery.c

--- boards/shields/nice_view_gem/widgets/battery.c
+++ boards/shields/nice_view_gem/widgets/battery.c
@@ -34,4 +34,9 @@ static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
 
 void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
     draw_level(canvas, state);
+    if (state->charging) {
+        lv_draw_img_dsc_t img_dsc;
+        lv_draw_img_dsc_init(&img_dsc);
+        lv_canvas_draw_img(canvas, 8, 10, &bolt, &img_dsc);
+    }
 }
